637471e9c6740bba989baf45b407b3b2fe84bccb
Eric O'Connell <eric.oconnell@idealist.org>
2014-05-27 23:39:55 -0400

    WIP: duration-based rendering close to working

--- src/canvas-profile.js
+++ src/canvas-profile.js
@@ -33,7 +33,7 @@ var App = {
             threshold: 20
         },
         hover: {
-            bgColor: 'rgba(0, 0, 0, 0.3)',
+            bgColor: 'rgba(0, 0, 0, 0.4)',
             color: '#fff'
         }
     },
@@ -61,10 +61,11 @@ var App = {
         this.profileContext = profile.getContext('2d');
         this.overlayContext = overlay.getContext('2d');
         this.render();
+        $('#width').val(this.state.xScale);
         $(overlay).on('mousewheel', function(e) {
             var xOffset = Math.min(
-                Math.max(this.state.xOffset + e.deltaX, 0),
-                this.samples.length - (Math.floor(window.innerWidth / this.state.xScale)));
+                Math.max(this.state.xOffset + e.deltaX * this.state.xScale / 100, 0),
+                this.samples.duration)
             this.setState({
                 xOffset: xOffset,
                 hovered: null
@@ -73,7 +74,7 @@ var App = {
         }.bind(this));
         $('#width').on('change', function(e) {
             var delta = {
-                xScale: parseInt(e.target.value, 10),
+                xScale: parseFloat(e.target.value, 10),
                 xOffset: this.state.xOffset,
                 hovered: null
             };
@@ -86,19 +87,19 @@ var App = {
             }
             this.setState(delta);
         }.bind(this));
-        $(overlay).on('mousemove', function(e) {
-            var x = Math.floor(e.offsetX / this.state.xScale) + this.state.xOffset;
-            var height = Math.floor(profile.offsetHeight / this.state.yScale);
-            var y = height - Math.floor(e.offsetY / this.state.yScale) - 1;
-            var delta = {hovered: this.samples[x][y]};
-            if (delta.hovered) {
-                delta.hoverX = e.offsetX;
-                delta.hoverY = e.offsetY;
-            } else {
-                delta.hovered = null;
-            }
-            this.setState(delta);
-        }.bind(this));
+        // $(overlay).on('mousemove', function(e) {
+        //     var x = Math.floor(e.offsetX / this.state.xScale) + this.state.xOffset;
+        //     var height = Math.floor(profile.offsetHeight / this.state.yScale);
+        //     var y = height - Math.floor(e.offsetY / this.state.yScale) - 1;
+        //     var delta = {hovered: this.samples[x][y]};
+        //     if (delta.hovered) {
+        //         delta.hoverX = e.offsetX;
+        //         delta.hoverY = e.offsetY;
+        //     } else {
+        //         delta.hovered = null;
+        //     }
+        //     this.setState(delta);
+        // }.bind(this));
         $(window).on('resize', function(e) {
             window.requestAnimationFrame(App.render.bind(this));
         }.bind(this));
@@ -117,9 +118,11 @@ var App = {
         var context = this.profileContext;
         var width = profile.width;
         var height = profile.height;
-        var samplesVisible = Math.floor(width / this.state.xScale);
+        // xScale is percent of samples visible
+        var timeVisible = this.samples.duration * (this.state.xScale * 0.01);
+        // TODO: rename this, possibly to xScale, thus renaming xScale to something like xZoom
+        var size = width / timeVisible;
         var yScale = this.state.yScale;
-        var xScale = this.state.xScale;
 
         // for offscreen text rendering
         var subcanvas = document.createElement('canvas');
@@ -130,23 +133,38 @@ var App = {
 
         var y = height;
 
+        var visibleSamples = [];
+        this.samples.bsp.root.inRange(this.state.xOffset,
+                                      this.state.xOffset + timeVisible,
+                                      function(s) { visibleSamples.push(s)});
+
+        // add the previous sample if need be
+        if (visibleSamples[0].t > this.state.xOffset) {
+            visibleSamples.unshift(processed[visibleSamples[0].index - 1]);
+        }
+
         for (var j = 0; j < this.samples.height; j++) {
-            var x = 0;
             y -= this.state.yScale;
 
             // set fill & stroke color once
             context.setFillColor(hsl(j * this.ui.hueShift, this.ui.fill.saturation, this.ui.fill.lightness));
             context.setStrokeColor(hsl(j * this.ui.hueShift, this.ui.stroke.saturation, this.ui.stroke.lightness));
 
-            for (var i = 0; i < samplesVisible; i++) {
-                var frame = this.samples[this.state.xOffset + i][j];
-                if (!frame) {
-                    x += xScale;
+            var tMax = -1;
+
+            for (var i = 0; i < visibleSamples.length; i++) {
+                var sample = visibleSamples[i];
+                var frame = sample[j];
+                if (!frame || sample.t < tMax) {
                     continue;
                 }
+                tMax = sample.t + frame.duration;
+
+                var x = (sample.t - this.state.xOffset) * size;
+                if (x < 0) { x = 0; }
                 var text = frame.fn;
 
-                var frameWidth = frame.width * xScale;
+                var frameWidth = frame.duration * size;
 
                 context.fillRect(x, y, frameWidth, yScale);
 
@@ -163,9 +181,6 @@ var App = {
                     context.drawImage(subcanvas, 0, 0, textDrawWidth, 16,
                                       x, y - 3, textDrawWidth, 16);
                 }
-
-                i += frame.width - 1;
-                x += xScale * frame.width;
             }
         }
 
@@ -185,7 +200,8 @@ var App = {
         var top = this.state.hoverY;
         var lines = [
             hovered.line ? hovered.line + ' [' + hovered.fn + ']' : hovered.fn,
-            hovered.file + ' (' + hovered.lineNumber + ')'
+            hovered.file + ' (' + hovered.lineNumber + ')',
+            (hovered.duration * 1000) + 'ms'
         ];
         var width = lines.reduce(function(max, line) {
             var width = this.measureText(line);
@@ -232,19 +248,34 @@ var App = {
         var height = samples.reduce(
             function(m, s) { return Math.max(m, s[3].length)}, 0);
 
-        processed = samples.map(function() { return Array(height); });
+        processed = samples.slice(0, width).map(function() { return Array(height); });
         processed.height = height;
+        processed.duration = samples[width][0] - samples[0][0];
+        processed.timeSampleMap = {};
+        console.log('total duration: ', processed.duration);
 
         // loop through all samples, set processed[sample][frame] to
+        // - processed[sample].t will be the time offset of that sample,
+        //   starting with processed[0].t == 0
         // - undefined if the stack was not that high OR
         // - a cell containing:
+        //     - the string representation of that function call
+        //     - the length of that function call as defined by the interval
+        //       processed[sample].t to processed[n].t where processed[n][frame]
+        //       represents the first different function call
+        //     DEPRECATED
         //     - the number of samples at this depth which represent
         //       the same function call
-        //     - the string representation of that function call
-        var duration = 0;
+
+        var initialTime = samples[0][0];
+
         for (var i = 0; i < width; i++) {
-            var stack = processed[i];
             var width;
+
+            // hints for our renderer to see where to start looking for samples
+            processed[i].t = samples[i][0] - initialTime;
+            processed[i].index = i;
+
             for (var j = 0; j < samples[i][3].length; j++) {
                 // skip if we have been here already
                 if (processed[i][j]) {
@@ -265,17 +296,24 @@ var App = {
 
                 // update the current cell width
                 cell.width = frameWidth;
+                var endTime = samples[k][0] - initialTime;
+                cell.duration = endTime - processed[i].t;
+
                 processed[i][j] = cell;
                 var w = frameWidth - 1;
 
                 // mark the width of the remaining cells
                 for (var k = i + 1; k < i + frameWidth; k++) {
                     processed[k][j].width = w--;
+                    // ugh, renormalize because this deals with processed & unprocessed times
+                    processed[k][j].duration = endTime - samples[k][0] + initialTime;
                 }
             }
         }
+
+        processed.bsp = new BSP(processed, {getter: function(sample) { return sample.t; } })
         console.timeEnd('prep');
-        console.log('total duration: ', duration);
+
         return processed;
     }
 }
